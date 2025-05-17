eb6ed69d75f2607027bb83be78be70c931094d2c
gabrieltrouve <gabrieltrouve5@gmail.com>
2025-05-17 11:39:53 +0200

    Refactor user model and update foreign key references to CustomUser; rename Magasin to Store and improve field naming conventions.
    Add AccountStore model

--- gestionstock/inventaire/models.py
+++ gestionstock/inventaire/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-from account.models import EnterpriseUser
+from account.models import CustomUser
 from django.conf import settings
 
 
-
+"""
+Il y a de l'anglais et français mélangé
+Utilise le snake_case pour les attributs
+"""
 
 class TypeEnterprise(models.Model):
     nom = models.CharField(max_length=100 )
@@ -22,7 +25,7 @@ class Enterprise(models.Model):
     nom = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     type_enterprise = models.ForeignKey(TypeEnterprise, on_delete=models.CASCADE)
-    User = models.ForeignKey(EnterpriseUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE) # Donc une entreprise appartient à un utilisateur ok
 
     def __str__(self):
         return f"{self.nom}-{self.type_enterprise.nom}"
@@ -30,12 +33,21 @@ class Enterprise(models.Model):
     def __repr__(self):
         return Enterprise(self.nom, self.address, self.type_enterprise)
 
-class Magasin(models.Model):
+class Store(models.Model): ## Mettre Store à la place de Magasin
     nom = models.CharField(max_length=100)
-    addresse = models.CharField(max_length=200)
-    idnumber = models.CharField(max_length=50)
-    entrepriseId = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
-    adminMagsin = models.ManyToManyField(to=settings.AUTH_USER_MODEL, verbose_name="Utilisateurs")
+    adresse = models.CharField(max_length=200)
+    id_number = models.CharField(max_length=50)
+    entreprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+
+
+class AccountStore(models.Model): ## Ici je ferais du coup AccountStore
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    role = models.ForeignKey("account.Role", on_delete=models.CASCADE) # ou ManyToMany s'il y a plusieurs roles possibles
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.store.nom} - {self.user.email}"
 
 
 class Product(models.Model):
@@ -50,15 +62,15 @@ class Product(models.Model):
         return f"Produit: {self.nom} de {self.sku} à un seuil alert {self.seuil_alert}"
 
     def __repr__(self):
-        return Product(self.SKU, self.nom, self.description, self.prix, self.seuil_alert)
+        return Product(self.sku, self.nom, self.description, self.prix, self.seuil_alert)
 
-class MagasinProduit(models.Model):
-    magasinId = models.ForeignKey(Magasin, on_delete=models.CASCADE)
-    produitId = models.ForeignKey(Product, on_delete=models.CASCADE)
+class StoreProduct(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 class Location(models.Model):
     produit_location = models.ForeignKey(Product, on_delete=models.CASCADE)
-    magasinId = models.ForeignKey(Magasin, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
     # ajouter le user
     date_location = models.DateTimeField(auto_now=True)
     nom = models.CharField("Nom", max_length=100)
@@ -79,7 +91,7 @@ class Mouvement(models.Model):
     type_mouvement = models.CharField(max_length=20, choices=MOVEMENT_CHOICES)
     produit = models.ForeignKey(Product, on_delete=models.CASCADE)
     lieu = models.CharField("Lieu", max_length=100)
-    datecreated = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now=True)
     raison = models.TextField("Raison")
 
     def __str__(self):
